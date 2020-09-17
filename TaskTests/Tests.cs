bb398ff056bb8a4a785ccb93a91eeb76ca36761d
RomanSPbGASU <romanevsin@yandex.ru>
2020-09-17 08:06:03 +0300

    Change variables' names according to the rules

--- TaskTests/Tests.cs
+++ TaskTests/Tests.cs
@@ -8,37 +8,37 @@ namespace TaskTests
     [TestFixture]
     public class Tests
     {
-        private Plugins pluginFactory;
-        private IPlugin adding;
-        private IPlugin subtracting;
+        private Plugins _pluginFactory;
+        private IPlugin _adding;
+        private IPlugin _subtracting;
         
         [SetUp]
         public void InitTests()
         {
-            pluginFactory = new Plugins();
-            adding = pluginFactory.GetPlugin("Addition");
-            subtracting = pluginFactory.GetPlugin("Subtraction");
+            _pluginFactory = new Plugins();
+            _adding = _pluginFactory.GetPlugin("Addition");
+            _subtracting = _pluginFactory.GetPlugin("Subtraction");
         }
         
         [Test]
         public void TestGetPlugin()
         {
-            Assert.IsInstanceOf(typeof(IPlugin), adding);
-            Assert.IsInstanceOf(typeof(IPlugin), subtracting);
-            Assert.Throws<Exception>(() => pluginFactory.GetPlugin("The wrong name"));
+            Assert.IsInstanceOf(typeof(IPlugin), _adding);
+            Assert.IsInstanceOf(typeof(IPlugin), _subtracting);
+            Assert.Throws<Exception>(() => _pluginFactory.GetPlugin("The wrong name"));
         }
         
         [Test]
         public void TestAddition()
         {
-            Assert.AreEqual(adding.Run(5, 8), 5 + 8);
-            Assert.Throws<OverflowException>(() => adding.Run(int.MaxValue, 1));
+            Assert.AreEqual(_adding.Run(5, 8), 5 + 8);
+            Assert.Throws<OverflowException>(() => _adding.Run(int.MaxValue, 1));
         }
         [Test]
         public void TestSubtraction()
         {
-            Assert.AreEqual(subtracting.Run(5, 8), 5 - 8);
-            Assert.Throws<OverflowException>(() => subtracting.Run(int.MinValue, 1));
+            Assert.AreEqual(_subtracting.Run(5, 8), 5 - 8);
+            Assert.Throws<OverflowException>(() => _subtracting.Run(int.MinValue, 1));
         }
     }
 }
