e18034292e8dc7ac659ea823537a32aff20e569a
RomanSPbGASU <romanevsin@yandex.ru>
2020-09-17 08:05:32 +0300

    Add docs

--- Ds_Do_Task/IPlugin.cs
+++ Ds_Do_Task/IPlugin.cs
@@ -1,12 +1,37 @@
 ﻿namespace ds.test.impl
 {
+    /// <summary>
+    /// Interface for plugins which execute mathematical operations
+    /// </summary>
     public interface IPlugin 
     { 
         
+        /// <summary>
+        /// Name of a plugin
+        /// </summary>
         string PluginName { get; } 
+        
+        /// <summary>
+        /// Version of a plugin
+        /// </summary>
         string Version { get; } 
+        
+        /// <summary>
+        /// Image that represent plugin
+        /// </summary>
         System.Drawing.Image Image { get; }
+        
+        /// <summary>
+        /// Description of a plugin
+        /// </summary>
         string Description { get; }
+        
+        /// <summary>
+        /// The method which execute plugin's primary action
+        /// </summary>
+        /// <param name="input1"></param>
+        /// <param name="input2"></param>
+        /// <returns></returns>
         int Run (int input1, int input2);
     }
 }
