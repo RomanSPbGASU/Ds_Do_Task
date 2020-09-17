614a864e6a24b3e7483d512230a83ab5d40f52c9
RomanSPbGASU <romanevsin@yandex.ru>
2020-09-17 07:21:43 +0300

    Add tests

---
+++ TaskTests/Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using NUnit.Framework;
+using ds.test.impl;
+
+namespace TaskTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Plugins pluginFactory;
+        private IPlugin adding;
+        private IPlugin subtracting;
+        
+        [SetUp]
+        public void InitTests()
+        {
+            pluginFactory = new Plugins();
+            adding = pluginFactory.GetPlugin("Addition");
+            subtracting = pluginFactory.GetPlugin("Subtraction");
+        }
+        
+        [Test]
+        public void TestGetPlugin()
+        {
+            Assert.IsInstanceOf(typeof(IPlugin), adding);
+            Assert.IsInstanceOf(typeof(IPlugin), subtracting);
+            Assert.Throws<Exception>(() => pluginFactory.GetPlugin("The wrong name"));
+        }
+        
+        [Test]
+        public void TestAddition()
+        {
+            Assert.AreEqual(adding.Run(5, 8), 5 + 8);
+            Assert.Throws<OverflowException>(() => adding.Run(int.MaxValue, 1));
+        }
+        [Test]
+        public void TestSubtraction()
+        {
+            Assert.AreEqual(subtracting.Run(5, 8), 5 - 8);
+            Assert.Throws<OverflowException>(() => subtracting.Run(int.MinValue, 1));
+        }
+    }
+}
